e081bf5cb04860424d49510e68d9e1a8ebd901c9
seungsu07 <haseungsu07@korea.ac.kr>
2026-04-03 15:00:04 +0900

    assets setup

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 
 app.get("/", (req, res) => {
-  res.send("Hello API")
+  res.send("Hello Korea University!")
 })
 
 app.listen(3000, () => {
