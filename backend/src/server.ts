035b32ce50f04a08b79df9f871b8398429087f2b
ritsucord <mangchya069@gmail.com>
2026-04-12 15:27:00 +0900

    webhook-test

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 import Express from "express";
 import { suspend } from "effection";
-import { z } from 'zod';
+import { z } from "zod";
 import { type ApiChannel } from "./api-base.js";
 
 export function* ServerManager(apiChannel: ApiChannel) {
@@ -8,24 +8,28 @@ export function* ServerManager(apiChannel: ApiChannel) {
     const port = 3000;
 
     const app = Express();
-    
+
     const SignUpScheme = z.object({
         id: z.string().min(6).max(18),
         password: z.string().min(6),
         name: z.string().min(2).max(6),
         student_id: z.string().length(10),
-        dept_id: z.string().length(4)
+        dept_id: z.string().length(4),
     });
 
-    app.all("/signup", (req, res) => {
-        function resolve(data: { status: number; ok: boolean }) {
-            res.status(data.status).json({ ok: data.ok });
-        }
-        function reject(data: { status: number; ok: boolean }) {
-            res.status(data.status).json({ ok: data.ok });
-        }
-        apiChannel.send({ data: req, resolve, reject });
+    app.all("/", (req, res) => {
+        res.json("OK");
     });
+
+    // app.all("/signup", (req, res) => {
+    //     function resolve(data: { status: number; ok: boolean }) {
+    //         res.status(data.status).json({ ok: data.ok });
+    //     }
+    //     function reject(data: { status: number; ok: boolean }) {
+    //         res.status(data.status).json({ ok: data.ok });
+    //     }
+    //     apiChannel.send({ data: req, resolve, reject });
+    // });
 
     app.listen(port, host, () => {
         console.log("Server is now running!");
@@ -33,4 +37,3 @@ export function* ServerManager(apiChannel: ApiChannel) {
 
     yield* suspend();
 }
-
