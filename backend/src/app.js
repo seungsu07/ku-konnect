3d4e0f47e2183c7a23d23aa0fb71bc5c54beddc3
ritsucord <mangchya069@gmail.com>
2026-05-11 16:17:06 +0900

    feat: enhance type safety and validation in API endpoints
    - Updated jsconfig.json to allow importing TypeScript extensions.
    - Modified package.json to enable experimental type stripping in Node.js.
    - Refactored app.js to implement type guards for request validation, improving error handling and data integrity.
    - Expanded dto.ts with additional type guards and refined type definitions for API routes.
    - Added a test HTTP request file for the signup endpoint to facilitate testing.

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,11 +3,12 @@
  * @import { RequestHandler } from 'express'
  * @import { RunningController } from './controller'
  * @import { ServerContext } from './server'
- * @import { Path, Scheme, PATH_ROUTE } from '../../common/dto'
+ * @import { Path, Scheme, PATH_ROUTE, TypeGuarder, TypeGuardObject, MethodOf } from '../../common/dto'
  */
 
 import express from 'express';
 import { Temporal } from '@js-temporal/polyfill';
+import { makeStringGuarder, makeNumberGuarder, makeBooleanGuarder, makeUndefinedGuarder, makeArrayGuarder, makeObjectGuarder } from '../../common/dto.ts';
 
 
 
@@ -24,6 +25,153 @@ import { Temporal } from '@js-temporal/polyfill';
  * @param {RunningController} rcon
  */
 export function CreateAppManager(context, rcon) {
+    /**
+     * @typedef {`${string}-${string}-${string}-${string}-${string}`} UUID
+     */
+    
+    /**
+     * @param {string} t
+     * @returns {t is UUID}
+     */
+    function checkUUID(t) {
+        return /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/.test(t);
+    }
+    
+    /**
+     * @template {any} E
+     * @param {E} e
+     * @returns {TypeGuarder<UUID, E>}
+     */
+    function makeUUIDGuarder(e) {
+        return (t) => typeof t == 'string' && checkUUID(t)? t: e;
+    }
+    
+    /**
+     * @constructor
+     * @param {string} message
+     * @param {string[]} path
+     * @property {string} message
+     * @property {string[]} path
+     */
+    function ParseError(message, path) {
+        this.message = message;
+        this.path = path;
+        return this;
+    }
+    
+    const G = {
+        s: makeStringGuarder(new ParseError('This field needed to be string', [])),
+        n: makeNumberGuarder(new ParseError('This field needed to be number', [])),
+        b: makeBooleanGuarder(new ParseError('This field needed to be boolean', [])),
+        u: makeUndefinedGuarder(new ParseError('This field needed to be undefined', [])),
+        id: makeUUIDGuarder(new ParseError('This field needed to be UUID', [])),
+        a: makeArrayGuarder(new ParseError('This field needed to be array', [])),
+        o: makeObjectGuarder(new ParseError('This field needed to be object', []))
+    };
+    
+    /**
+     * @template T, E
+     * @param {TypeGuardObject<T, E>} o
+     * @returns {TypeGuarder<T, E>}
+     */
+    function solveGuardObject(o) {
+        return (t) => {
+            const obg = G.o(t);
+            if (obg instanceof ParseError)
+                return new ParseError(obg.message, []);
+            const entries = Object.entries(o).map(([k, v]) => [k, v(t[k])]);
+            const errIndex= entries.findIndex(([_, v]) => v instanceof ParseError);
+            let error;
+            if (errIndex != -1) {
+                /** @type {[string, ParseError]} */
+                const [k, v] =/** @type {any} */ (entries[errIndex]);
+                error = new ParseError(v.message, v.path.concat([k]));
+            }
+            return error? error: Object.fromEntries(entries)
+        };
+    }
+    
+    /** @type {{ [K in Path]: { [L in MethodOf<K>]: TypeGuarder<Scheme<K, L, 'REQ'>, ParseError> } }} */
+    const guarders = {
+        '/api/auth/login': {
+            POST: solveGuardObject({
+                id: G.id,
+                password: G.s
+            })
+        },
+        '/api/auth/signup': {
+            POST: solveGuardObject({
+                campus: G.id,
+                college: G.id,
+                department: G.id,
+                student_id: (t) =>
+                    /^20[12][0-9]{7}$/.test(t)? t:
+                    new ParseError('This field needed to be student id', []),
+                name: G.s,
+                login_id: G.s,
+                password: G.s,
+                univ_mail: solveGuardObject({
+                    address:  (t) =>
+                        /^.+@[0-9A-z]+\.[0-9A-z]{2,6}$/.test(t)? t:
+                        new ParseError('This field needed to be email address', []),
+                    token: G.id
+                })
+            })
+        },
+        '/api/data/timetable': {
+            GET: solveGuardObject({
+                id: G.id
+            })
+        },
+        '/api/auth/verify/mail': {
+            GET: solveGuardObject({
+                address: (t) =>
+                    /^.+@[0-9A-z]+\.[0-9A-z]{2,6}$/.test(t)? t:
+                    new ParseError('This field needed to be email address', [])
+            }),
+            POST: solveGuardObject({
+                address:  (t) =>
+                    /^.+@[0-9A-z]+\.[0-9A-z]{2,6}$/.test(t)? t:
+                    new ParseError('This field needed to be email address', []),
+                code: (t) =>
+                    /[0-9]{6}/.test(t)? t:
+                    new ParseError('This field needed to be 6-digit code', [])
+            })
+        }
+    };
+    
+    /**
+     * @template {Path} T
+     * @template {MethodOf<T>} U
+     * @template V
+     * @param {T} path
+     * @param {U} method
+     * @param {RequestHandler<V, Scheme<T, U, 'RES'>, Scheme<T, U, 'REQ'>>} fn
+     * @returns {[T, RequestHandler<V, Scheme<T, U, 'RES'>, Scheme<T, U, 'REQ'>>]}
+     */
+    function makeHandler(path, method, fn) {
+        /** @type {RequestHandler<V, Scheme<T, U, 'RES'>, Scheme<T, U, 'REQ'>>} */
+        function handler(req, res, next) {
+            const data = method == 'GET'?
+                req.query: req.body;
+            const guarder = guarders[path][method];
+            /** @type {ParseError | Scheme<T, U, 'REQ'>} */
+            const gdata = /** @type {any} */ (guarder(data));
+            if (gdata instanceof ParseError) {
+                res.status(400).json(/** @type {any} */ ({
+                    success: false,
+                    e: `${gdata.message}${gdata.path.length?
+                        ', at \'' + gdata.path.reverse().join('.') + '\'': ''}`
+                }));
+                return;
+            }
+            req.body = gdata;
+            fn(req, res, next);
+            return;
+        }
+        return [path, handler];
+    }
+
     const app = express();
 
     app.use(express.json());
@@ -35,61 +183,13 @@ export function CreateAppManager(context, rcon) {
         }
     );
     
-    /**
-     * @typedef {`${string}-${string}-${string}-${string}-${string}`} UUID
-     */
-    
-    /**
-     * @param {string} uuid
-     * @returns {uuid is UUID}
-     */
-    function checkUUID(uuid) {
-        return /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/.test(uuid);
-    }
-    
-    /**
-     * @template {Path} T
-     * @template {keyof PATH_ROUTE[T]} U
-     * @template {any} V
-     * @param {T} path
-     * @param {U} method
-     * @param {RequestHandler<V, Scheme<T, U, 'RES'>, Scheme<T, U, 'REQ'>>} fn
-     * @returns {[T, RequestHandler<V, Scheme<T, U, 'RES'>, Scheme<T, U, 'REQ'>]}
-     */
-    function handler(path, method, fn) {
-        /** @type {RequestHandler} */
-        function h(req, res) {
-            
-        }
-    }
-    
-    const test = handler('/api/auth/signup', 'POST', (req, res) => {
-        req.body.name
-    });
-
-    app.post('/auth/signup', (req, res) => {
-        /** @type {Record<string, unknown>} */
-        const {
-            campus,
-            college,
-            department,
-            student_id,
-            name,
-            login_id,
-            password,
-            univ_mail
-        } = req.body;
-        if (
-            typeof campus != 'string' ||
-            !checkUUID(campus)
-        ) {
-            res.status(400).json()
-        }
-    });
+    app.post(...makeHandler('/api/auth/signup', 'POST', (req, res, next) => {
+        res.status(200).end('congrats.');
+    }));
 
     app.use(/** @type {express.ErrorRequestHandler} */
         (err, req, res, next) => {
-            res.status(500).json({ e: 'unexpected' });
+            res.status(500).json({ e: 'unexpected', err });
             return;
         }
     );
@@ -99,6 +199,7 @@ export function CreateAppManager(context, rcon) {
         
         async serve(delay=Temporal.Duration.from({ minutes: 3 })) {
             const controller = rcon.inner;
+            app.listen(3000);
             await controller.start();
             
             while (true) {
--- common/dto.ts
+++ common/dto.ts
@@ -1,15 +1,4 @@
-import { EntityID, Preferences, TimeTable } from './models';
-
-export type Method =
-  | 'GET'
-  | 'POST'
-  | 'PATCH'
-  | 'PUT'
-  | 'DELETE';
-
-export type Kind =
-  | 'REQ'
-  | 'RES';
+import { type EntityID, type Preferences, type TimeTable } from './models.ts';
 
 export type ErrorString =
   | 'bad_request'
@@ -26,7 +15,42 @@ export type Route =
   | { success: true; }
   | { success: false; e: ErrorString; };
 
-export interface PATH_ROUTE {
+export type Path =
+  | '/api/auth/verify/mail'
+  // | '/api/auth/verify/tel'
+  | '/api/auth/signup'
+  | '/api/auth/login'
+  | '/api/data/timetable'
+  // | '/api/generate/timetable';
+
+export type Method =
+  | 'GET'
+  | 'POST'
+  | 'PATCH'
+  | 'PUT'
+  | 'DELETE';
+
+export type Kind =
+  | 'REQ'
+  | 'RES';
+
+export type MethodOf<T extends Path> = {
+  [K in keyof PATH_ROUTE[T]]: PATH_ROUTE[T][K] extends undefined?
+    never: K;
+}[keyof PATH_ROUTE[T]];
+
+export type KindOf<T extends Path, U extends MethodOf<T>> =
+  keyof PATH_ROUTE[T][U];
+
+type PATH_ROUTE_SCHEME = {
+  [K in Path]: {
+    [L in Method]?: {
+      [M in Kind]: any;
+    }
+  };
+};
+
+export interface PATH_ROUTE extends PATH_ROUTE_SCHEME {
   '/api/auth/verify/mail': {
     GET: {
       REQ: {
@@ -139,32 +163,62 @@ export interface PATH_ROUTE {
   };
 }
 
-export type Path = keyof PATH_ROUTE;
-
 export type Scheme<
   T extends Path,
-  U extends keyof PATH_ROUTE[T] | undefined = undefined,
+  U extends MethodOf<T> | undefined = undefined,
   V extends Kind | undefined = undefined
 > =
-  U extends keyof PATH_ROUTE[T]?
-    (V extends keyof PATH_ROUTE[T][U]?
+  U extends MethodOf<T>?
+    (V extends KindOf<T, U>?
       PATH_ROUTE[T][U][V]:
       PATH_ROUTE[T][U]):
     PATH_ROUTE[T];
 
 export type RouteFunction<
   T extends Path,
-  U extends keyof PATH_ROUTE[T],
+  U extends MethodOf<T>,
   V = any[],
   W = {}
 > = (data: Scheme<T, U, 'REQ'>, ...args: V extends any[]? V: [V]) => Scheme<T, U, 'RES'> & W;
 
-export type TypeGuarder<T> =
-  T extends string? StringConstructor:
-  T extends number? NumberConstructor:
-  T extends boolean? BooleanConstructor:
-  T extends bigint? BigIntConstructor:
-  { new (...args: any[]): T } | ((...args: any[]) => T);
+export type TypeGuardObject<T, E = never> =
+  // T extends string | number | boolean | bigint | symbol | null | undefined? TypeGuarder<T, E>:
+  { [K in keyof T]: TypeGuarder<T[K], E> };
 
-export type TypeGuarderObject<T> =
-  { [K in keyof T]: TypeGuarder<T[K]>; };
+export type TypeGuarder<T, E = never> = (arg: any) => T | E;
+
+export function makeStringGuarder<E>(e: E): TypeGuarder<string, E> {
+  return (t) => typeof t == 'string'? t: e;
+}
+
+export function makeNumberGuarder<E>(e: E): TypeGuarder<number, E> {
+  return (t) => typeof t == 'number'? t: e;
+}
+
+export function makeBooleanGuarder<E>(e: E): TypeGuarder<boolean, E> {
+  return (t) => typeof t == 'boolean'? t: e;
+}
+
+export function makeBigintGuarder<E>(e: E): TypeGuarder<bigint, E> {
+  return (t) => typeof t == 'bigint'? t: e;
+}
+
+export function makeSymbolGuarder<E>(e: E): TypeGuarder<symbol, E> {
+  return (t) => typeof t == 'symbol'? t: e;
+}
+
+export function makeNullGuarder<E>(e: E): TypeGuarder<null, E> {
+  return (t) => t === null? t: e;
+}
+
+export function makeUndefinedGuarder<E>(e: E): TypeGuarder<undefined, E> {
+  return (t) => typeof t == 'undefined'? t: e;
+}
+
+export function makeArrayGuarder<E>(e: E): TypeGuarder<any[], E> {
+  return (t) => Array.isArray(t)? t: e;
+}
+
+export function makeObjectGuarder<E = any>(e: E): TypeGuarder<object, E> {
+  return (t) => typeof t == 'object'? t === null? e: t: e;
+}
